23dbf47c94ce0c2489fc05f05b3a71ae25081f43
I'm Alagar <alagar17302@gmail.com>
2023-10-17 07:53:56 +0530

    third

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const element = document.getElementById('yourElementId');
+if (element) {
+  // Access the classList property here
+  element.classList.add('some-class');
+} else {
+  console.error("Element not found");
+}
